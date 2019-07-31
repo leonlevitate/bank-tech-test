1b0d544870e91bbf1db3d716e9417e20b73f462f
Leon Linton <leon.linton@live.co.uk>
2019-07-31 10:26:14 +0100

    Adds Doubles for Testing

--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -5,18 +5,19 @@ require 'bank_statement'
 RSpec.describe BankStatement do
   let(:bank_statement) { BankStatement.new }
   let(:date) { Time.now.strftime('%d/%m/%Y') }
+  let(:statement1) { double :statement1 }
 
   it 'generates a bank statement with formatting' do
-    statement = [{ date: date, credit: 2000, debit: nil, balance: 2000 },
+    statement1 = [{ date: date, credit: 2000, debit: nil, balance: 2000 },
                  { date: date, credit: nil, debit: 100, balance: 1900 },
                  { date: date, credit: 400, debit: nil, balance: 2300 }]
-    expect(bank_statement.create_statement(statement)).to eq [{ date: date, credit: 2000, debit: nil, balance: 2000 },
+    expect(bank_statement.create_statement(statement1)).to eq [{ date: date, credit: 2000, debit: nil, balance: 2000 },
                                                               { date: date, credit: nil, debit: 100, balance: 1900 },
                                                               { date: date, credit: 400, debit: nil, balance: 2300 }]
   end
 
   it 'creates a bank statement' do
     expect(bank_statement).to receive(:create_statement)
-    bank_statement.create_statement(@statement)
+    bank_statement.create_statement(statement1)
   end
 end
